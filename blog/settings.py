583cecdfd5842ab58283a9f502bd82fc3299bbfe
kimtaehyeon <thxogusrla@gmail.com>
2019-07-28 21:44:15 +0900

    second commit

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '=+)scac7_4-vrm@cp)imb!+g)*33!x
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
